8f36e096e8ac4e9178ba6176b274a7f6bdf7e4d5
NguyenLeQuyen2004 <quyennlph37238@fpt.edu.vn>
2024-03-21 11:54:31 +0700

    update db.json

--- src/components/Silde.tsx
+++ src/components/Silde.tsx
@@ -5,17 +5,11 @@ function DarkVariantExample() {
     <Carousel data-bs-theme='dark'>
       <Carousel.Item>
         <img className='d-block w-100' src='https://anphat.com.vn/media/lib/29-04-2023/laptop.jpg' alt='First slide' />
-        <Carousel.Caption>
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
+        <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img className='d-block w-100' src='https://anphat.com.vn/media/lib/29-04-2023/pc.jpg' alt='Second slide' />
-        <Carousel.Caption>
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
+        <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
@@ -23,10 +17,7 @@ function DarkVariantExample() {
           src='https://anphat.com.vn/media/lib/08-05-2023/build-pc-banner.jpg'
           alt='Third slide'
         />
-        <Carousel.Caption>
-          <h5>Third slide label</h5>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </Carousel.Caption>
+        <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   )
